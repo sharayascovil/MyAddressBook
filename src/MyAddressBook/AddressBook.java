2455512f1ae934e64f10bca054818d65efbc8751
Duwe Aynab <mfaynab88@gmail.com>
2022-03-31 19:35:47 -0400

    Missing main page. Working on refactoring.

---
+++ src/MyAddressBook/AddressBook.java
@@ -0,0 +1,49 @@
+package MyAddressBook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private String bookName;
+    private List<Person> addresses;
+    private int inputQuantity;
+
+    public AddressBook(String bookName) {
+        this.bookName = bookName;
+        this.inputQuantity = 0;
+        addresses = new ArrayList<>();
+
+    }
+
+    public void addInput(Person person) {
+        addresses.add(person);
+        inputQuantity++;
+    }
+
+    public void deleteInput(int index) {
+        addresses.remove(index);
+        inputQuantity--;
+    }
+
+    public void deleteInput(Person person) {
+        addresses.remove(person);
+        inputQuantity--;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public List<Person> getAddresses() {
+        return addresses;
+    }
+
+    public int getInputQuantity() {
+        return inputQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return ("Book \"" + bookName + "\" with " + inputQuantity + "input(s)");
+    }
+}
